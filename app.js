b73411ef433af709215e8d57fab37c7906485955
tatsuya <kurururainwater@gmail.com>
2018-06-06 01:30:45 +0900

    4000に修正

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000);
+}).listen(4000);
  
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:4000/');
